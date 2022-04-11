71cb6c2b63d3fcf33a7270f15be10aa0e303abc7
SpencerNikolaeff <spni5258@colorado.edu>
2022-04-11 13:49:59 -0600

    adding file structure

---
+++ VendingMachine/src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -0,0 +1,14 @@
+package vendingmachine.controller;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 17202
+ */
+public class VendingMachineController {
+    
+}
