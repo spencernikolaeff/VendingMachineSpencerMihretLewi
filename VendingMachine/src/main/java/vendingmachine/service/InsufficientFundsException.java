a7ca73d8d109974bb8b24291c53fbccd20da5f38
Lewi Adrong <lewiadrong@gmail.com>
2022-04-12 15:54:25 -0400

    Added new exception classes

---
+++ VendingMachine/src/main/java/vendingmachine/service/InsufficientFundsException.java
@@ -0,0 +1,12 @@
+package vendingmachine.service;
+
+public class InsufficientFundsException extends Exception {
+
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+
+    public InsufficientFundsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ VendingMachine/src/main/java/vendingmachine/service/NoItemInventoryException.java
@@ -0,0 +1,12 @@
+package vendingmachine.service;
+
+public class NoItemInventoryException extends Exception {
+
+    public NoItemInventoryException(String message) {
+        super(message);
+    }
+
+    public NoItemInventoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
