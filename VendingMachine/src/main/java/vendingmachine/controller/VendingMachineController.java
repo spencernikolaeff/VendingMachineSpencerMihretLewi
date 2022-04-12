c5019434199c24590cb366d776deb0c298c4b491
SpencerNikolaeff <spni5258@colorado.edu>
2022-04-11 18:55:27 -0600

    added txt file with preset drinks, added basic controller

--- VendingMachine/src/main/java/vendingmachine/controller/VendingMachineController.java
+++ VendingMachine/src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,5 +1,10 @@
 package vendingmachine.controller;
 
+import vendingmachine.dao.VendingMachineFI;
+import vendingmachine.ui.UserIO;
+import vendingmachine.ui.UserIOConsoleImpl;
+import vendingmachine.ui.VendingMachineView;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,5 +15,80 @@ package vendingmachine.controller;
  * @author 17202
  */
 public class VendingMachineController {
+    UserIO uio = new UserIOConsoleImpl();
+    private VendingMachineFI dao = new VendingMachineDaoFI();
+    private VendingMachineView view = new VendingMachineView(uio);
+    
+    boolean programRunning = true;
+    
+    public void run() {
+        
+        while(programRunning) {
+            
+            dao.loadDrinks();
+            int choice = view.printMenuAndGet();
+            
+            switch(choice) {
+                
+                case 1:
+                    listDrinks();
+                    break;
+                case 2:
+                    enterMoney();
+                    break;
+                case 3:
+                    viewBalance();
+                    break;
+                case 4:
+                    viewDrink();
+                    break;
+                case 5:
+                    purchaseDrink();
+                    break;
+                case 0:
+                    exit();
+                    break;
+                default:
+                    errorMessage();
+            }
+            System.out.println();
+            System.out.println();
+        }
+    }
+    
+    //list drinks
+    private void listDrinks() {
+        
+    }
+    
+    //enter money
+    private void enterMoney() {
+        
+    }
+    
+    //view balance
+    private void viewBalance() {
+        
+    }
+    
+    //view drink
+    private void viewDrink() {
+        
+    }
+    
+    //purchase drink
+    private void purchaseDrink() {
+        
+    }
+    
+    //exit
+    private void exit() {
+        
+    }
+    
+    //error message
+    private void errorMessage() {
+        System.out.println("Error Message");
+    }
     
 }
--- VendingMachine/src/main/java/vendingmachine/dto/Drink.java
+++ VendingMachine/src/main/java/vendingmachine/dto/Drink.java
@@ -8,6 +8,57 @@ package vendingmachine.dto;
  *
  * @author 17202
  */
-public class Drink {
+public abstract class Drink {
+    
+    //attributes
+    private String name;
+    private int pricePenny; //pennies
+    private int quantity;
+    
+    //basic constructor
+    public Drink(String name){
+        this.name = name;
+    }
+    
+    //full constructor
+    public Drink(String name, int price, int amount) {
+        this.name = name;
+        this.pricePenny = price;
+        this.quantity = amount;
+    }
+    
+    //getters
+    //
+    //these are public since they'll need to be accessed outside the class
+    //
+    public String getName() {
+        return name;
+    }
+    
+    public int getPricePenny() {
+        return pricePenny;
+    }
+    
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    //setters
+    //
+    //these are private and only used in DrinkFactory?
+    //--------i couldn't figure out Java factory classes
+    //maybe not necessary since we can just load the drinks from a file anyways
+    //
+    private void setName(String name){
+        this.name = name;
+    }
+    
+    private void setPrice(int price){
+        this.pricePenny = price;
+    }
+    
+    private void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
     
 }
