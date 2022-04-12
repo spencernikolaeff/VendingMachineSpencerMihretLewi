93853b405f794ea4ef1cd6e279984323eba018db
Mihret Tadesse <mihrettadesse@Mihrets-MBP.attlocal.net>
2022-04-11 21:03:56 -0400

    userio

--- VendingMachine/src/main/java/vendingmachine/ui/UserIO.java
+++ VendingMachine/src/main/java/vendingmachine/ui/UserIO.java
@@ -9,5 +9,24 @@ package vendingmachine.ui;
  * @author 17202
  */
 public interface UserIO {
-    
+
+    public void print(String main_Menu);
+
+    public double readDouble(String prompt);
+
+    public double readDouble(String prompt, double min, double max);
+
+    public float readFloat(String prompt);
+
+    public float readFloat(String prompt, float min, float max);
+
+    public int readInt(String prompt);
+
+    public int readInt(String prompt, int min, int max);
+
+    public long readLong(String prompt);
+
+    public long readLong(String prompt, long min, long max);
+
+    String readString(String prompt);
 }
