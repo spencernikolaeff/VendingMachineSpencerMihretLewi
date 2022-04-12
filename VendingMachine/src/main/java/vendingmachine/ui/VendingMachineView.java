b1784e39159e52a253902d58e22d18e2b348767f
Mihret Tadesse <mihrettadesse@Mihrets-MBP.attlocal.net>
2022-04-11 21:01:56 -0400

    vending macjine view

--- VendingMachine/src/main/java/vendingmachine/ui/VendingMachineView.java
+++ VendingMachine/src/main/java/vendingmachine/ui/VendingMachineView.java
@@ -9,5 +9,32 @@ package vendingmachine.ui;
  * @author 17202
  */
 public class VendingMachineView {
+
+    final private UserIO io;
+
+    public VendingMachineView(UserIO io) {
+        this.io = io;
+    }
+
+    //main menu
+    public int printMenuAndGet() {
+        io.print("Main Menu");
+        io.print("1. List drink");
+        io.print("2. View balance");
+        io.print("3. View drink ");
+        io.print("4. Enter Money");
+        io.print("5. Purchase drink ");
+        io.print("0. Exit");
+
+        return io.readInt("Please select from the above choices.", 0, 5);
+    }
+    public String listTheProduct() {
+        return io.readString("Please enter the product list :"); 
+        
+    }
+    public String listTheprice() {
+        return io.readString("Please enter the product price :"); 
+        
+    }
     
 }
