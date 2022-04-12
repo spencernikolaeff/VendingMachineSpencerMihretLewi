2f745c9f1f64090e67fbfb45aa06b6d88cb25216
Mihret Tadesse <mihrettadesse@Mihrets-MBP.attlocal.net>
2022-04-12 14:54:06 -0400

    change in vending Machine code

--- VendingMachine/src/main/java/vendingmachine/ui/VendingMachineView.java
+++ VendingMachine/src/main/java/vendingmachine/ui/VendingMachineView.java
@@ -41,13 +41,12 @@ public class VendingMachineView {
         return io.readString("Please enter the drinkName :");
 
     }
-    
+
     public void displayDrink(Drink name) {
         io.print("[Name] " + name.getName() + " [Price] " + name.getPricePenny() + " [Quantity] " + name.getQuantity());
     }
-    
+
     //buyDrink
-    
     public boolean BuyDrink() {
         String userChoice = io.readString("Would you like to purchase this Drink? (y/n)");
         return userChoice.equals("y") || userChoice.equals("Y");
@@ -71,16 +70,16 @@ public class VendingMachineView {
         io.readString(
                 "drink successfully added to your cart.  Please hit enter to continue");
     }
-    
+
     //check if they have enough money messages
     public void displayEnoughMoney(boolean enough) {
-        if(enough) {
+        if (enough) {
             System.out.println("Drink Purchased Successfully");
         } else {
             System.out.println("You need to enter more money to purchase this drink...");
         }
     }
-    
+
     //displayAllDrink
     public void displayAllDrinkBanner() {
         System.out.println("==== All Drinks ====");
@@ -90,56 +89,64 @@ public class VendingMachineView {
         for (Drink currDrink : DrinkList) {
             String DrinkInfo = String.format("[Name] %s  ",
                     currDrink.getName());
-                    io.print(DrinkInfo);
+            io.print(DrinkInfo);
         }
         io.readString("Press Enter to continue.");
     }
-    
+
     public void displayErrorMessage(String error) {
         System.out.println("Recieved Error: " + error);
     }
-    
+
     //view balance
     public void viewBalanceBanner() {
         System.out.println("==== View Balance ====");
     }
-    
+
     public void viewBalance(double n) {
         System.out.println("Current Balance: $" + n);
     }
-    
+
     //exit
     public void exitMessage() {
         System.out.println("Goodbye! Don't forget to take your drinks!");
     }
-    
+
     //list available drinks 
     public void listAvailableDrinksBanner1() {
         System.out.println("==== Affordable Drinks ====");
     }
+
     //banner two to show selected price
     public void listAvailableDrinksBanner2(Double amount) {
         System.out.println("==== Drinks For $" + amount + " or less ====");
     }
+
     //read input price for benchmark value
     public Double getAvailableDrinksPrice() {
         return io.readDouble("Please Enter a price to get drinks at or below that price");
     }
-    
+
     //error message
-    public void errorMessage(String error){
+    public void errorMessage(String error) {
         io.print("Error Message: " + error);
     }
-     public void withdrawMoneyBanner() {
+
+    public void withdrawMoneyBanner() {
         System.out.println("==== Withdraw Money ====");
     }
-     public void showAllCoinBanner() {
+
+    public void showAllCoinBanner() {
         System.out.println("==== Your Coins  ====");
     }
-     
+
     //error message
-     public void withdrawMoney(boolean enough, Change withdraw){
-         
-     }
-    
+    public void withdrawMoney(boolean enough, Change withdraw) {
+        if (enough) {
+            System.out.println("==== Transaction is successfull. Your change is : " + withdraw.getTotal());
+        } else {
+            System.out.println("==== Transaction is unsuccessfull ====");
+        }
+    }
+
 }
